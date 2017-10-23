4b5f5b83562aaa0aa04be9864cf933efe7a48461
Ilija Stanojkovic <ilijas@student.ethz.ch>
2017-10-23 12:47:40 +0200

    Refactoring to flake8 standard

--- ml_project/models/feature_selection.py
+++ ml_project/models/feature_selection.py
@@ -5,6 +5,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 class HistogramDownsampling(BaseEstimator, TransformerMixin):
     """Downsampling data with histogram"""
+
     def __init__(self, bins=10, kernel_size=(16, 16)):
         self.bins = bins
         self.kernel_size = kernel_size
@@ -15,18 +16,31 @@ class HistogramDownsampling(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         X_reshaped = X.reshape(-1, 176, 208, 176)
         X_cropped = X_reshaped[:, 40:140, 40:170, 40:140]
-        no_histograms = math.ceil(X_cropped.shape[2] / self.kernel_size[0]) * math.ceil(X_cropped.shape[3] / self.kernel_size[1])
-        X_hist = np.zeros(shape=[X_cropped.shape[0], X_cropped.shape[1], no_histograms, self.bins])
+        no_histograms = math.ceil(X_cropped.shape[2] / self.kernel_size[0]) * \
+            math.ceil(X_cropped.shape[3] / self.kernel_size[1])
+        X_hist = np.zeros(shape=[X_cropped.shape[0],
+                                 X_cropped.shape[1],
+                                 no_histograms,
+                                 self.bins])
         for sample_index in range(X_cropped.shape[0]):
             print((sample_index + 1) / X_cropped.shape[0] * 100)
             for layer_index in range(X_cropped.shape[1]):
                 layer = X_cropped[sample_index][layer_index]
-                no_j_buckets = math.ceil(X_cropped.shape[3] / self.kernel_size[1])
+                no_j_buckets = math.ceil(X_cropped.shape[3] /
+                                         self.kernel_size[1])
                 for bucket_i in range(0, layer.shape[0], self.kernel_size[0]):
-                    for bucket_j in range(0, layer.shape[1], self.kernel_size[1]):
-                        bucket = layer[bucket_i:min(bucket_i + self.kernel_size[0], layer.shape[0]),
-                                 bucket_j:min(bucket_j + self.kernel_size[1], layer.shape[1])]
-                        hist, bin_edges = np.histogram(bucket.reshape(-1), bins=self.bins)
-                        hist_index = (bucket_i // self.kernel_size[0]) * no_j_buckets + (bucket_j // self.kernel_size[1])
+                    for bucket_j in range(0,
+                                          layer.shape[1],
+                                          self.kernel_size[1]):
+                        bucket_i_end = min(bucket_i + self.kernel_size[0],
+                                           layer.shape[0])
+                        bucket_j_end = min(bucket_j + self.kernel_size[1],
+                                           layer.shape[1])
+                        bucket = layer[bucket_i:bucket_i_end,
+                                       bucket_j:bucket_j_end]
+                        hist, bin_edges = np.histogram(bucket.reshape(-1),
+                                                       bins=self.bins)
+                        hist_index = (bucket_i // self.kernel_size[0]) * \
+                            no_j_buckets + (bucket_j // self.kernel_size[1])
                         X_hist[sample_index][layer_index][hist_index] = hist
         return X_hist.reshape(X_hist.shape[0], -1)
